7ec738b786d286a1f3eb18379bcd69a479105279
Alwan Ibrahim <alwanfi4525010@univpancasila.ac.id>
2026-04-29 13:03:50 +0700

    Praktik ke3

---
+++ PASD3-5.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Data {
+    string nama;
+    int umur;
+};
+
+struct Node {
+    Data info;
+    Node* next;
+};
+
+struct Stack {
+    Node* top;
+};
+
+void buatStackKosong(Stack *s) {
+    s->top = NULL;
+}
+
+bool isKosong(Stack s) {
+    return s.top == NULL;
+}
+
+void push(string nama, int umur, Stack *s) {
+    Node* baru = new Node;
+    baru->info.nama = nama;
+    baru->info.umur = umur;
+    baru->next = s->top;
+    s->top = baru;
+}
+
+void tampil(Stack s) {
+    if (isKosong(s)) {
+        cout << "Stack kosong\n";
+        return;
+    }
+
+    Node* bantu = s.top;
+    while (bantu != NULL) {
+        cout << bantu->info.nama << " --" << bantu->info.umur << endl;
+        bantu = bantu->next;
+    }
+}
+
+void hapusTiara(Stack *s) {
+    Node *bantu = s->top, *prev = NULL;
+
+    while (bantu != NULL) {
+        if (bantu->info.nama == "Tiara" && bantu->info.umur == 63) {
+            if (prev == NULL) {
+                s->top = bantu->next;
+            } else {
+                prev->next = bantu->next;
+            }
+            delete bantu;
+            return;
+        }
+        prev = bantu;
+        bantu = bantu->next;
+    }
+}
+
+int main() {
+    Stack S;
+    buatStackKosong(&S);
+
+    push("Candra", 22, &S);
+    push("Cinthya", 66, &S);
+    push("Ilham", 28, &S);
+    push("Tiara", 63, &S);
+    push("Fanny", 10, &S);
+
+    cout << "Sebelum penghapusan:\n";
+    tampil(S);
+
+    hapusTiara(&S);
+
+    cout << "\nSetelah penghapusan Tiara --63:\n";
+    tampil(S);
+
+    return 0;
+}
---
+++ PASD3-7.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node {
+    string info;
+    Node* next;
+};
+
+struct Stack {
+    Node* top;
+};
+
+void buatKosong(Stack *s) {
+    s->top = NULL;
+}
+
+void pushDepan(string x, Stack *s) {
+    Node* baru = new Node;
+    baru->info = x;
+    baru->next = s->top;
+    s->top = baru;
+}
+
+void tampil(Stack s) {
+    Node* bantu = s.top;
+    while (bantu != NULL) {
+        cout << bantu->info << endl;
+        bantu = bantu->next;
+    }
+}
+
+Node* cari(string x, Stack s) {
+    Node* bantu = s.top;
+    while (bantu != NULL) {
+        if (bantu->info == x) return bantu;
+        bantu = bantu->next;
+    }
+    return NULL;
+}
+
+void sisipSetelah(string setelah, string dataBaru, Stack *s) {
+    Node* p = cari(setelah, *s);
+    if (p != NULL) {
+        Node* baru = new Node;
+        baru->info = dataBaru;
+        baru->next = p->next;
+        p->next = baru;
+    }
+}
+
+void tambahPalingBawah(string dataBaru, Stack *s) {
+    Node* baru = new Node;
+    baru->info = dataBaru;
+    baru->next = NULL;
+
+    if (s->top == NULL) {
+        s->top = baru;
+        return;
+    }
+
+    Node* bantu = s->top;
+    while (bantu->next != NULL) {
+        bantu = bantu->next;
+    }
+    bantu->next = baru;
+}
+
+int main() {
+    Stack S;
+    buatKosong(&S);
+
+    pushDepan("Maemo", &S);
+    pushDepan("Symbian", &S);
+    pushDepan("Mac", &S);
+    pushDepan("Windows", &S);
+    pushDepan("Iphone", &S);
+
+    sisipSetelah("Iphone", "Iphone 5", &S);
+    sisipSetelah("Windows", "Windows Phone", &S);
+    sisipSetelah("Mac", "SnowLeopard", &S);
+    sisipSetelah("Symbian", "Symbian Belle", &S);
+    tambahPalingBawah("Maemo 4", &S);
+
+    tampil(S);
+    return 0;
+}
